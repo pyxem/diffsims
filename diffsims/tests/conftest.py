c3946b11c72b41085c854cde4bc9a1a713979739
Duncan Johnstone <duncanjohnstone@live.co.uk>
2019-07-15 17:26:49 +0100

    update to diffsims in test_library and test_utils

--- diffsims/tests/conftest.py
+++ diffsims/tests/conftest.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2019 The pyXem developers
+# Copyright 2017-2019 The diffsims developers
 #
-# This file is part of pyXem.
+# This file is part of diffsims.
 #
-# pyXem is free software: you can redistribute it and/or modify
+# diffsims is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pyXem is distributed in the hope that it will be useful,
+# diffsims is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+# along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
 import diffpy.structure
 import numpy as np
 from transforms3d.euler import euler2mat
 
-from pyxem.signals.electron_diffraction import ElectronDiffraction
-from pyxem.libraries.vector_library import DiffractionVectorLibrary
+from diffsims.libraries.vector_library import DiffractionVectorLibrary
 
 
 @pytest.fixture
