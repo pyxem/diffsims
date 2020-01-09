57974630e313bca13874ac7c0efe3977e0e955de
pc494 <pc494@cam.ac.uk>
2020-01-09 17:49:27 +0000

    Minor internal change addressing #37

--- diffsims/utils/sim_utils.py
+++ diffsims/utils/sim_utils.py
@@ -399,9 +399,9 @@ def get_points_in_sphere(reciprocal_lattice, reciprocal_radius):
         Distance of reciprocal lattice points in sphere from the origin.
     """
     a, b, c = reciprocal_lattice.a, reciprocal_lattice.b, reciprocal_lattice.c
-    h_max = np.ceil(reciprocal_radius / a)
-    k_max = np.ceil(reciprocal_radius / b)
-    l_max = np.ceil(reciprocal_radius / c)
+    h_max = np.floor(reciprocal_radius / a)
+    k_max = np.floor(reciprocal_radius / b)
+    l_max = np.floor(reciprocal_radius / c)
     from itertools import product
     h_list = np.arange(-h_max, h_max + 1) #arange has a non-inclusive endpoint
     k_list = np.arange(-k_max, k_max + 1)
