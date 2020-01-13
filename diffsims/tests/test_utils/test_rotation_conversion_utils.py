fcd2986c5fd80c584ce523a6e67dc3eb00a5a707
pc494 <pc494@cam.ac.uk>
2020-01-13 20:24:11 +0000

    First commit of test for vectorised euler2quat

---
+++ diffsims/tests/test_utils/test_rotation_conversion_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2019 The diffsims developers
+#
+# This file is part of diffsims.
+#
+# diffsims is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffsims is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+import numpy as np
+
+from transforms3d.euler import euler2quat
+from diffsims.utils.rotation_conversion_utils import vectorised_euler2quat,vectorised_quat2axangle
+
+
+
+@pytest.fixture()
+def random_eulers():
+    """ Using [0,360] [0,180] and [0,360] as ranges """
+    alpha = np.random.rand(100) * 360
+    beta  = np.random.rand(100) * 180
+    gamma = np.random.rand(100) * 360
+    eulers = np.asarray((alpha,beta,gamma)).T
+    return np.deg2rad(eulers)
+
+def test_vectorised_euler2quat(random_eulers):
+    fast = vectorised_euler2quat(random_eulers[:,0],random_eulers[:,1],random_eulers[:,2],'rzxz')
+
+    stored_quats = np.ones((random_eulers.shape[0],4))
+    for i,row in enumerate(random_eulers):
+        temp_quat = euler2quat(row[0],row[1],row[2],'rzxz')
+        for j in [0,1,2,3]:
+            stored_quats[i,j] = temp_quat[j]
+
+    assert np.allclose(fast,stored_quats)
