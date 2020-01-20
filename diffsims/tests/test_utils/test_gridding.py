bfdbfec8cd7a8929f076c7b35c31b1ee8b19e25b
pc494 <pc494@cam.ac.uk>
2020-01-20 11:20:23 +0000

    first commit of tests for get_grid_around_beam_direction

--- diffsims/tests/test_utils/test_gridding.py
+++ diffsims/tests/test_utils/test_gridding.py
@@ -17,8 +17,28 @@
 # along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
-from diffsims.utils.gridding import get_local_grid
+import numpy as np
+from diffsims.utils.gridding import get_local_grid, get_grid_around_beam_direction
+from diffsims.utils.gridding_utils import Euler
+
 
 def test_get_local_grid():
-    z = get_local_grid((0,0,0),10,1)
-    assert True
+    grid = get_local_grid((0,0,0),10,2)
+    assert isinstance(grid,list)
+    assert isinstance(grid[0],tuple)
+
+def test_get_grid_around_beam_direction():
+    grid_simple = get_grid_around_beam_direction((0,0,0),1,(0, 360))
+    assert isinstance(grid_simple,list)
+    assert isinstance(grid_simple[0],tuple)
+    assert len(grid_simple) == 360
+
+    grid_rotated = get_grid_around_beam_direction((0,90,0),1,(0, 360))
+    assert isinstance(grid_simple,list)
+    assert isinstance(grid_simple[0],tuple)
+    assert len(grid_simple) == 360
+
+    axangle = Euler(np.asarray(grid_rotated),axis_convention='rzxz').to_AxAngle()
+    assert np.allclose(axangle.data[1:,0][axangle.data[1:,3] > 0],0)
+    assert np.allclose(axangle.data[1:,1][axangle.data[1:,3] > 0],1)
+    assert np.allclose(axangle.data[1:,2][axangle.data[1:,3] > 0],0)
