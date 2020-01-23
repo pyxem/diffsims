8d8f2c362afc4164500e2f9e97783c9f6a4a5e69
pc494 <pc494@cam.ac.uk>
2020-01-23 17:07:01 +0000

    axis --> axes

--- diffsims/utils/gridding_utils.py
+++ diffsims/utils/gridding_utils.py
@@ -204,9 +204,9 @@ def get_beam_directions(resolution,crystal_system,equal='angle'):
         pass
 
     points_in_cartesians = vectorised_spherical_polars_to_cartesians(points_in_spherical_polars)
-    axis = np.cross([0,0,1],points_in_cartesians) #in unit cartesians so this is fine, [0,0,1] returns [0,0,0]
+    axes = np.cross([0,0,1],points_in_cartesians) #in unit cartesians so this is fine, [0,0,1] returns [0,0,0]
 
-    angle = np.arcsin(np.linalg.norm(axis,axis=1))
-    normalised_axis = np.divide(axis,np.linalg.norm(axis,axis=1).reshape(-1,1))
-    eulers = AxAngle(np.hstack((normalised_axis,angle.reshape((-1,1))))).to_Euler(axis_convention='rzxz')
+    angle = np.arcsin(np.linalg.norm(axes,axis=1))
+    normalised_axes = np.divide(axes,np.linalg.norm(axes,axis=1).reshape(-1,1))
+    eulers = AxAngle(np.hstack((normalised_axes,angle.reshape((-1,1))))).to_Euler(axis_convention='rzxz')
     return eulers
