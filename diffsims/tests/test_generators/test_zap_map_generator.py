48d1b7746a3307f93221e968efd6e018032cae8e
pc494 <pc494@cam.ac.uk>
2020-02-04 13:35:19 +0000

    Gets the tests running

--- diffsims/tests/test_generators/test_zap_map_generator.py
+++ diffsims/tests/test_generators/test_zap_map_generator.py
@@ -18,6 +18,8 @@
 
 import pytest
 
+import diffpy.structure
+import numpy as np
 from diffsims.generators.zap_map_generator import get_rotation_from_z
 
 @pytest.fixture
@@ -28,29 +30,40 @@ def cubic():
     atom = diffpy.structure.atom.Atom(atype='Ni',xyz=[0,0,0],lattice=latt)
     return diffpy.structure.Structure(atoms=[atom],lattice=latt)
 
+
 @pytest.mark.parametrize("sample_system",[cubic])
 def test_zero_rotation_cases(sample_system):
     r_test = get_rotation_from_z(sample_system,[0,0,2])
     assert r_test == (0,0,0)
 
 
-@pytest.mark.parametrize("sample_system",[cubic])
-class TestOrthonormals:
+
+
+class TestOrthonormals():
+
+    @pytest.fixture(params=[(3,3,3),(3,3,4),(3,4,5)])
+    def sample_system(self,request):
+        """An atomic structure represented using diffpy
+        """
+        a,b,c = request.param[0],request.param[1],request.param[2]
+        latt = diffpy.structure.lattice.Lattice(a,b,c,90,90,90)
+        atom = diffpy.structure.atom.Atom(atype='Ni',xyz=[0,0,0],lattice=latt)
+        return diffpy.structure.Structure(atoms=[atom],lattice=latt)
 
     def test_rotation_to_x_axis(self,sample_system):
         r_to_x = get_rotation_from_z(sample_system,[0,1,0])
-        assert np.allclose(r_test,(0,90,0))
+        assert np.allclose(r_to_x,(0,90,0))
 
     def test_rotation_to_y_axis(self,sample_system):
         r_to_y = get_rotation_from_z(sample_system,[1,0,0])
-        assert np.allclose(r_test,(90,90,0))
+        assert np.allclose(r_to_y,(90,90,0))
 
     def test_rotations_to_yz(self,sample_system):
         """ We rotate from z towards y, in the cubic case the angle
         will be 45, ---- """
         r_to_yz = get_rotation_from_z(sample_system,[0,1,1])
         cos_angle = np.cos(np.deg2rad(r_to_yz[2]))
-        cos_lattice = sample_system.b / sample_system.c
+        cos_lattice = sample_system.lattice.b / sample_system.lattice.c
         assert cos_angle == cos_lattice
 
     def test_rotation_to_111(self,sample_system):
