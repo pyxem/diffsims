d86d512d205488f43cff7ebee065b199d3a51b76
phillip <pc494@cam.ac.uk>
2020-07-21 12:58:03 +0100

    bump version to 0.3.0dev

--- diffsims/release_info.py
+++ diffsims/release_info.py
@@ -1,5 +1,5 @@
 name = "diffsims"
-version = "0.2.3"
+version = "0.3.0dev"
 author = "Duncan Johnstone, Phillip Crout"
 copyright = "Copyright 2017-2020, The pyXem Developers"
 credits = ["Duncan Johnstone", "Phillip Crout", "Ben Martineau", "Simon Hogas"]
