7d3f113d966328b991d21c69acaf01cb4836c509
phillip <pc494@cam.ac.uk>
2020-08-27 15:31:25 +0100

    Adds some missing copyright notices

--- diffsims/tests/test_utils/test_fourier_transform.py
+++ diffsims/tests/test_utils/test_fourier_transform.py
@@ -1,8 +1,20 @@
-"""
-Created on 2 Nov 2019
-
-@author: Rob Tovey
-"""
+# -*- coding: utf-8 -*-
+# Copyright 2017-2020 The diffsims developers
+#
+# This file is part of diffsims.
+#
+# diffsims is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffsims is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
 import numpy as np
