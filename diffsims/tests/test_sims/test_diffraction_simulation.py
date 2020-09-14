861194f2d9438104e04928d8bf329f5bcca4a0ba
phillip <pc494@cam.ac.uk>
2020-09-14 15:16:40 +0100

    Correcting test for g_max being removed

--- diffsims/tests/test_sims/test_diffraction_simulation.py
+++ diffsims/tests/test_sims/test_diffraction_simulation.py
@@ -83,7 +83,7 @@ def profile_simulation():
 
 
 def test_plot_profile_simulation(profile_simulation):
-    profile_simulation.get_plot(g_max=1)
+    profile_simulation.get_plot()
 
 
 class TestDiffractionSimulation:
