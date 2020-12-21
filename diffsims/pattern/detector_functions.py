3b6a1b31b6bef28bd5af163a5a13b46e36e16d60
phillip <pc494@cam.ac.uk>
2020-12-21 16:49:33 +0000

    Finishing starter detector functions

--- diffsims/pattern/detector_functions.py
+++ diffsims/pattern/detector_functions.py
@@ -16,32 +16,51 @@
 # You should have received a copy of the GNU General Public License
 # along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
 
-
+from numpy.random import default_rng
 from scipy import ndimage as ndi
 
 def add_gaussian_blur(pattern,sigma):
     """
+    Blurs intensities with a gaussian function
 
     Parameters
     ----------
     pattern : np.array
-
+        The diffraction pattern at the detector
     sigma : float
-          The standard deviation of the gaussian blur, in pixels
+        The standard deviation of the gaussian blur, in pixels
+
     Returns
     -------
     blurred_pattern : np.array
-
-    Notes
-    -----
-    This blurring, as well as being inuitive is often a good first model
-    of a detector response function [source]
-
-    References
-    ----------
-    [source]
+        The blurred pattern (deterministic)
     """
     return ndi.gaussian_filter(pattern, sigma)
 
-def add_shot_noise():
-    pass
+def add_shot_noise(pattern,seed=None):
+    """
+    Applies shot noise to a pattern
+
+    Parameters
+    ----------
+    pattern : np.array
+        The diffraction pattern at the detector
+    seed : int or None
+        seed value for the random number generator
+
+    Returns
+    -------
+    shotted_pattern : np.array
+        A single sample of the pattern after accounting for shot noise
+
+    Notes
+    -----
+    This function will (as it should) behave differently depending on the
+    pattern intensity, so be mindful to put your intensities in physical units
+    """
+    if seed is not None:
+        rng = default_rng(seed)
+    else:
+        rng = default_rng()
+
+    return rng.poisson(z)
