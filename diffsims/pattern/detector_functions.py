c870d9bff99621d7b83dfe64a4b0ded1fd93513d
phillip <pc494@cam.ac.uk>
2020-12-22 18:07:47 +0000

    Completing detector_function additions

--- diffsims/pattern/detector_functions.py
+++ diffsims/pattern/detector_functions.py
@@ -120,32 +120,81 @@ def add_gaussian_noise(pattern,sigma,seed=None):
 
 def add_dead_pixels(pattern,n=None,fraction=None,seed=None):
     """
+    Adds randomly placed dead pixels onto a pattern
+
+    Parameters
+    ----------
+    pattern : np.array
+        The diffraction pattern at the detector
+    n : int
+        The number of dead pixels, defaults to None
+    fraction : float
+        The fraction of dead pixels, defaults to None
+    seed : int or None
+        seed value for the random number generator
+
+    Returns
+    -------
+    corrupted_pattern : np.array
+        The pattern, with dead pixels included
     """
-    rng = _process_seed_argument(seed)
+    # sorting the n/fraction kwargs
 
-    if n is not None and fraction is not None:
-        raise ValueError("One of n and fraction must be None")
+    both_none = n is None and fraction is None
+    neither_none =  n is not None and fraction is not None
 
+    if both_none or neither_none:
+        raise ValueError("Exactly one of 'n' and 'fraction' must be None")
+
+    # converting fraction to n if needs be
     if fraction is not None:
         pattern_size = pattern.shape[0] * pattern.shape[1]
         n = int(fraction * pattern_size)
 
-    #xdead =
-    #ydead =
+    rng = _process_seed_argument(seed)
 
-    #pattern[xdead,ydead] = 0
+    # .astype rounds down, these generate values from 0 to (pattern.shape - 1)
+    xdead = rng.uniform(low=0,high=pattern.shape[0],size=n).astype(int)
+    ydead = rng.uniform(low=0,high=pattern.shape[1],size=n).astype(int)
+
+    pattern[xdead,ydead] = 0
 
     return pattern
 
 
 def add_linear_detector_gain(pattern,gain):
     """
+    Multiplies the pattern by a gain (which is not a function of the pattern)
+
+    Parameters
+    ----------
+    pattern : np.array
+        The diffraction pattern at the detector
+    gain : float or np.array
+        Multiplied through the pattern, broadcasting applies
+    Returns
+    -------
+    corrupted_pattern : np.array
+        The pattern, with gain applied
     """
     return np.multiply(pattern,gain)
 
 
 def add_detector_offset(pattern,offset):
     """
+    Adds/subtracts a fixed offset value from a pattern
+
+    Parameters
+    ----------
+    pattern : np.array
+        The diffraction pattern at the detector
+    offset : float or np.array
+        Added through the pattern, broadcasting applies
+    Returns
+    -------
+    corrupted_pattern : np.array
+        The pattern, with offset applied, pixels that would have been negative
+        are instead 0.
     """
     pattern = np.add(pattern,offset)
     return constrain_to_dynamic_range(pattern)
