42bc5620d2e71a433b3c11356e921afd654da1e4
phillip <pc494@cam.ac.uk>
2021-04-14 17:18:33 +0100

    Tidying up the full integral case

--- diffsims/generators/diffraction_generator.py
+++ diffsims/generators/diffraction_generator.py
@@ -50,49 +50,8 @@ _shape_factor_model_mapping = {
     "lorentzian": lorentzian,
 }
 
-
-def _z_sphere_precession(theta, r_spot, wavelength, phi):
-    """
-    Returns the z-coordinate in reciprocal space of the Ewald sphere at
-    distance r_spot from the origin
-
-    Parameters
-    ----------
-    theta : float
-        The azimuthal angle in degrees
-    r_spot : float
-        The projected length of the reciprocal lattice vector onto the plane
-        perpendicular to the optical axis in A^-1
-    wavelength : float
-        The electron wavelength in A
-    phi : float
-        The precession angle in degrees (angle between beam and optical axis)
-
-    Returns
-    -------
-    z : float
-        The height of the ewald sphere at the point r in A^-1
-
-    Notes
-    -----
-    * The azimuthal angle is the angle the beam is currently precessed to.
-    It is the angle between the projection of the beam and the projection of
-    the relevant diffraction spot both onto the x-y plane.
-    * In the derivation of this formula we assume that we will always integrate
-    over a full precession circle, because we do not explicitly take into
-    consideration x-y coordinates of reflections.
-    """
-    theta = np.deg2rad(theta)
-    r = 1 / wavelength
-    phi = np.deg2rad(phi)
-    return -np.sqrt(
-        r ** 2 * (1 - np.sin(phi) ** 2 * np.sin(theta) ** 2)
-        - (r_spot - r * np.sin(phi) * np.cos(theta)) ** 2
-    ) + r * np.cos(phi)
-
-
 def _shape_factor_precession(
-    z_spot, r_spot, wavelength, phi, shape_function, max_excitation, **kwargs
+    excitation_error, r_spot, wavelength, phi, shape_function, max_excitation, **kwargs
 ):
     """
     The rel-rod shape factors for reflections taking into account
@@ -100,12 +59,10 @@ def _shape_factor_precession(
 
     Parameters
     ----------
-    z_spot : np.ndarray (N,)
-        An array representing the z-coordinates of the reflections in A^-1
+    excitation_error : np.ndarray (N,)
+        An array of excitation errors
     r_spot : np.ndarray (N,)
         An array representing the distance of spots from the z-axis in A^-1
-    wavelength : float
-        The electron wavelength in A
     phi : float
         The precession angle in degrees
     shape_function : callable
@@ -130,33 +87,17 @@ def _shape_factor_precession(
     """
     shf = np.zeros(z_spot.shape)
     # loop over all spots
-    for i, (z_spot_i, r_spot_i) in enumerate(zip(z_spot, r_spot)):
+    for i, (excitation_error_i, r_spot_i) in enumerate(zip(excitation_error, r_spot)):
 
-        def integrand(phi):
-            z_sph = _z_sphere_precession(phi, r_spot_i, wavelength, phi)
-            return shape_function(z_spot_i - z_sph, max_excitation, **kwargs)
+        def integrand(theta):
+            # Equation 8 in L.Palatinus et al. Acta Cryst. (2019) B75, 512-522
+            S_zero = z_spot_i - z_sph
+            variable_term = r_spot_i*np.deg2rad(phi)*np.cos(theta)
+            return shape_function(S_zero + variable_term, max_excitation, **kwargs)
 
         # average factor integrated over the full revolution of the beam
-        shf[i] = (1 / (360)) * quad(integrand, 0, 360)[0]
+        shf[i] = (1 / (2*np.pi)) * quad(integrand, 0, 2*np.pi)[0]
     return shf
-
-
-def _average_excitation_error_precession(z_spot, r_spot, wavelength, precession_angle):
-    """
-    Calculate the average excitation error for spots
-    """
-    ext = np.zeros(z_spot.shape)
-    # loop over all spots
-    for i, (z_spot_i, r_spot_i) in enumerate(zip(z_spot, r_spot)):
-
-        def integrand(phi):
-            z_sph = _z_sphere_precession(phi, r_spot_i, wavelength, precession_angle)
-            return z_spot_i - z_sph
-
-        # average factor integrated over the full revolution of the beam
-        ext[i] = (1 / (360)) * quad(integrand, 0, 360)[0]
-    return ext
-
 
 class DiffractionGenerator(object):
     """Computes electron diffraction patterns for a crystal structure.
@@ -326,9 +267,8 @@ class DiffractionGenerator(object):
                 )
             else:
                 shape_factor = _shape_factor_precession(
-                    intersection_coordinates[:, 2],
+                    excitation_error,
                     r_spot,
-                    wavelength,
                     self.precession_angle,
                     self.shape_factor_model,
                     max_excitation_error,
