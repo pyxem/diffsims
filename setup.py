1598b68be43796aaeeeb05984b2b84c75b126910
Carter Francis <csfrancis@wisc.edu>
2023-11-15 08:28:30 -0600

    Fix test failures
    Fix test failures

--- setup.py
+++ setup.py
@@ -75,13 +75,13 @@ setup(
     packages=find_packages(),
     extras_require=extra_feature_requirements,
     install_requires=[
-        "diffpy.structure   >= 3.0.0",  # First Python 3 support
+        "diffpy.structure   >= 3.0.2",  # First Python 3 support
         "matplotlib         >= 3.3",
         "numba",
         "numpy              >= 1.17",
         "orix               >= 0.11",
         "psutil",
-        "scipy              >= 1.1",
+        "scipy              >= 1.2",
         "tqdm               >= 4.9",
         "transforms3d",
     ],
