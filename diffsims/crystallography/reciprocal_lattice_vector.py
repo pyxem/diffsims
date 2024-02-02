BugFix: Fix euler rotation

diffsims/crystallography/reciprocal_lattice_vector.py
@@ -533,7 +533,7 @@ class ReciprocalLatticeVector(Vector3d):
 
     def rotate_from_matrix(self, rotation_matrix):
         return ReciprocalLatticeVector(
-            phase=self.phase, xyz=np.matmul(rotation_matrix, self.data.T).T
+            phase=self.phase, xyz=np.matmul(rotation_matrix.T, self.data.T).T
         )
 
     @property

diffsims/simulations/simulation2d.py
@@ -507,7 +507,7 @@ class Simulation2D:
 
         if show_labels:
             millers = np.round(
-                np.matmul(coords.hkl, self.get_current_rotation())
+                np.matmul(coords.hkl, self.get_current_rotation().T)
             ).astype(np.int16)
             # only label the points inside the axes
             xlim = ax.get_xlim()