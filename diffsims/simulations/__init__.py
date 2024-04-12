1fc6a876a9372afa9035c0894dc7a393311dc657
Carter Francis <csfrancis@wisc.edu>
2024-04-12 17:35:13 -0500

    New Feature:  Add Simulation classes using Phase Class

---
+++ diffsims/simulations/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2023 The diffsims developers
+#
+# This file is part of diffsims.
+#
+# diffsims is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffsims is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffsims.  If not, see <http://www.gnu.org/licenses/>.
+
+from diffsims.simulations.simulation2d import Simulation2D
+from diffsims.simulations.simulation1d import Simulation1D
